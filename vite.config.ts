31358dac947c92400553cece5eafc50d4f74d5db
hervi <hervipro@gmail.com>
2025-12-21 10:59:45 +0700

    chore : Delete vite config PWA dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      srcDir: 'public',
-      filename: 'firebase-messaging-sw.js',
-      injectRegister: false, // Karena kita register manual
-      manifest: false, // Tidak perlu manifest PWA
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-    }),
   ],
   resolve: {
     alias: {
